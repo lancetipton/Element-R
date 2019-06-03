fd1f7800cfb9af23489b3443e8085e43c0bfe406
lancetipton <lancetipton04@gmail.com>
2019-06-02 17:03:05 -0700

    Cleaned up webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const webpack = require('webpack')
 
 const libraryName = 'elementR'
 const ENV_MODE = process.env.ENV
